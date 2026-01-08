926a877c446d07f13c486621f700580558db9e6a
Iraida-8 <aldo.sanchez@palosgarzalogistics.com>
2026-01-08 17:47:41 +0000

    notas

--- pages/01. Consulta.py
+++ pages/01. Consulta.py
@@ -2,7 +2,7 @@ import streamlit as st
 from datetime import date
 
 # =================================
-# Page configuration
+# Page configuration fecha, tipo de unidad, unidad camion id y empresa,   apartado descripcion, refaciones data apartado ostes = descripcion para mano obra
 # =================================
 st.set_page_config(
     page_title="Captura Pase de Taller",
