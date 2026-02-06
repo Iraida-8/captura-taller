d1a433a5cc27c46df7f25c22eec01d7583eba4ee
Iraida-8 <aldo.sanchez@palosgarzalogistics.com>
2026-02-06 17:16:29 -0600

    ass

--- pages/1_ Consultar Reparacion.py
+++ pages/1_ Consultar Reparacion.py
@@ -242,7 +242,7 @@ if not df_partes.empty and "Unidad" in df_partes.columns:
     display_total_col = None
 
     if empresa == "IGLOO TRANSPORT":
-        total_col = "Total Correcion"
+        total_col = "Total Correccion"
         display_total_col = "Total Correccion"
     elif empresa == "LINCOLN FREIGHT":
         total_col = "Total USD"
