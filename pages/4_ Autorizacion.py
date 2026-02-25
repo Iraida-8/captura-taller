35c31fe76aa99cf8d1b1cc4664e11777371f2f70
Iraida-8 <aldo.sanchez@palosgarzalogistics.com>
2026-02-25 09:31:16 -0600

    retry

--- pages/4_ Autorizacion.py
+++ pages/4_ Autorizacion.py
@@ -17,7 +17,7 @@ fecha_mod = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 def get_supabase_client():
     return create_client(
         st.secrets["SUPABASE_URL"],
-        st.secrets["SUPABASE_KEY"]
+        st.secrets["SUPABASE_ANON_KEY"]
     )
 
 # =================================
