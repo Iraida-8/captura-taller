99e9e45e83cb5a2c986c8e87ad3e4a9c4bb8283f
Iraida-8 <aldo.sanchez@palosgarzalogistics.com>
2026-07-15 09:35:38 -0600

    size

--- pages/4_ Autorizacion Beta.py
+++ pages/4_ Autorizacion Beta.py
@@ -909,7 +909,7 @@ with left:
             </div>
             """
 
-            components.html(estado_html, height=300)
+            components.html(estado_html, height=330)
 
     # ====================================================
     # TAB 3 - FACTURACIÓN
