8ab7d653d18f863d992938a2015edd5b45aaa1af
Iraida-8 <aldo.sanchez@palosgarzalogistics.com>
2026-08-31 15:36:19 -0600

    re

--- pages/11_ api_pull.py
+++ pages/11_ api_pull.py
@@ -2831,23 +2831,23 @@ with tab_historial:
 
                         MILES_TO_KM = 1.609344
 
-                        if "trip_distance" in trip_df.columns:
-                            trip_df["trip_distance"] = (
-                                trip_df["trip_distance"]
-                                * MILES_TO_KM
-                            )
+                        for col in [
+                            "trip_distance",
+                            "max_speed",
+                            "avg_speed",
+                            "fastest_distance"
+                        ]:
 
-                        if "max_speed" in trip_df.columns:
-                            trip_df["max_speed"] = (
-                                trip_df["max_speed"]
-                                * MILES_TO_KM
-                            )
+                            if col in trip_df.columns:
 
-                        if "avg_speed" in trip_df.columns:
-                            trip_df["avg_speed"] = (
-                                trip_df["avg_speed"]
-                                * MILES_TO_KM
-                            )
+                                trip_df[col] = pd.to_numeric(
+                                    trip_df[col],
+                                    errors="coerce"
+                                ).fillna(0)
+
+                                trip_df[col] = (
+                                    trip_df[col] * MILES_TO_KM
+                                )
 
 
                     # =====================================
