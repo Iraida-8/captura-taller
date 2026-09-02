facd02eda29d12b1ff717fc0a8ffdba7d24b5fb9
Iraida-8 <aldo.sanchez@palosgarzalogistics.com>
2026-09-02 12:25:07 -0600

    aa

--- pages/4_ Autorizacion Beta.py
+++ pages/4_ Autorizacion Beta.py
@@ -4935,6 +4935,19 @@ if has_viaticos:
                                     montos_solicitados
                                 ) if montos_solicitados else "$0.00"
 
+                                conceptos_solicitud = (
+                                    solicitud_row.get(
+                                        "conceptos",
+                                        []
+                                    )
+                                )
+
+                                if not isinstance(
+                                    conceptos_solicitud,
+                                    list
+                                ):
+                                    conceptos_solicitud = []
+
                                 st.markdown(
                                     f"""
                                     <div style='
